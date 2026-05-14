201bf4da8b8b9636d6e2cc73d30eb22d38ff9400
20231201647 <2744638717@qq.com>
2026-05-14 18:26:21 +0800

    第一次修改

---
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -0,0 +1,26 @@
+package com.sky.handler;
+
+import com.sky.exception.AccountAlreadyExistsException;
+import com.sky.exception.BaseException;
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public Result exceptionHandler(BaseException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error(ex.getMessage());
+    }
+
+    @ExceptionHandler(AccountAlreadyExistsException.class)
+    public Result exceptionHandler(AccountAlreadyExistsException ex){
+        log.error("异常信息：{}", ex.getMessage());
+        return Result.error(ex.getMessage());
+    }
+
+}
